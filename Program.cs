f3c6bf624e56f59f1af47cb591e0a5059bfd9374
Maddox Macnamara <tpmac001maddog@gmail.com>
2023-02-27 10:14:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Part_6___Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string selection;
+            do
+            {
+                Console.WriteLine("Who is the better captain?");
+                Console.WriteLine("a - Captain Picard");
+                Console.WriteLine("b - Captain Kirk");
+                selection = Console.ReadLine();
+            } while (selection != "a");
+            Console.WriteLine("Correct");
+        }
+    }
+}
