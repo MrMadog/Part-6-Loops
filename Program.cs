a5176b41169d9f678b171e06421a5148cd8950b8
Maddox Macnamara <tpmac001maddog@gmail.com>
2023-03-02 11:03:49 -0500

    messing with loops, doing some exercizes

--- Program.cs
+++ Program.cs
@@ -5,14 +5,49 @@
         static void Main(string[] args)
         {
             string selection;
+            bool done, done1;
+            done = false;
+            done1 = false;
+            double price;
+            int age;
+            /*
             do
             {
-                Console.WriteLine("Who is the better captain?");
-                Console.WriteLine("a - Captain Picard");
-                Console.WriteLine("b - Captain Kirk");
+                Console.WriteLine("Who has a better captain? ");
+                Console.WriteLine("a - Captain Kirk");
+                Console.WriteLine("b - Captain Pircard");
                 selection = Console.ReadLine();
             } while (selection != "a");
-            Console.WriteLine("Correct");
-        }
+
+
+            do
+            {
+                Console.WriteLine("Hello");
+                Console.WriteLine("How much money do you have?");
+                if (Double.TryParse(Console.ReadLine(), out price))
+                {
+                    done = true;
+                }
+
+            }while (done != true);
+
+            Console.WriteLine(price);
+            */
+
+            while (done1 != true)
+            {
+                Console.Write("How old are you? ");
+                if (Int32.TryParse(Console.ReadLine(), out age))
+                {
+                    if (age <= 0)
+                    {
+                    }
+                    else
+                    {
+                        done1 = true;
+                    }
+                }
+            }
+        }   
     }
 }
