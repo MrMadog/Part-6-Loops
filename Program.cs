406b654e04f02265f627ec505d9b759a1afe046c
Maddox Macnamara <tpmac001maddog@gmail.com>
2023-03-08 11:03:11 -0500

    finished up Loops Part 6

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Security;
+﻿using Part_5._5___More_Classes;
+using System.Net.Security;
 
 namespace Part_6___Loops
 {
@@ -9,7 +10,7 @@ namespace Part_6___Loops
             string program;
             bool done = false;
 
-            while (done != true)
+            while (!done)
             {
                 Console.WriteLine();
                 Console.WriteLine("MAIN MENU");
@@ -157,9 +158,10 @@ namespace Part_6___Loops
             static void Program2()
             {
                 bool done = false;
-                int score, answers;
-                double percent;
-                answers = 0;
+                int score, i;
+                double percent, valid;
+                i = 0;
+                valid = 0;
 
                 Console.WriteLine();
                 Console.WriteLine("Running Percent Passer... ");
@@ -168,30 +170,75 @@ namespace Part_6___Loops
                 Console.WriteLine();
 
 
-                Console.WriteLine("Enter a negative number to quit. ");
+                Console.WriteLine("You are going to be entering test scores (0 - 100). ");
+                Thread.Sleep(1000);
+                Console.WriteLine();
+
+                Console.WriteLine("Enter a negative number if you would like to quit. ");
+                Thread.Sleep(1500);
+                Console.WriteLine();
+
                 do
                 {
-                    Console.WriteLine("Enter a score:  ");
+                    Console.Write("Enter a score:  ");
 
-                    score = Convert.ToInt32(Console.ReadLine());
+                    while (!Int32.TryParse(Console.ReadLine(), out score))
+                    {
+                        Console.WriteLine("Invalid Input. ");
+                        Console.WriteLine();
+                        Console.Write("Enter a score:  ");
+                    }
+
 
                     if (score < 0)
                     {
+                        Console.WriteLine();
                         Console.WriteLine("Quitting... ");
+                        Thread.Sleep(1000);
                         done = true;
                     }
-                    else if (score >= 0 && score <= 100)
-                    {
-                        answers += 1;
 
-                    }
-                    else
+                    if (score >= 0 && score <= 100)
                     {
-                        Console.WriteLine("Invalid Input");
+                        i += 1;
+
+
+                        if (score >= 70 && score <= 100)
+                        {
+                            valid += 1;
+                        }
+                    }
+                    else if (score > 100)
+                    {
+                        Console.WriteLine("Invalid Input. ");
+                        Console.WriteLine();
                     }
 
-                } while(!done);
-                Console.WriteLine(answers);
+                    percent = valid / i;
+
+                    if (percent >= 1)
+                    {
+                        percent = 1;
+                    }
+                    else if (percent <= 0)
+                    {
+                        percent = 0;
+                    }
+
+                    percent = percent * 100;
+                    percent = Math.Round(percent, 2);
+
+                } while (!done);
+
+                if (i >= 1)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"{percent}% of your given scores were above a 70%! ");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Program finished, press ENTER to continue to menu");
+                Console.ReadLine();
             }
 
 // PROGRAM THREE ------------------------------------------------------------------------------------------------------------------------------------
@@ -234,6 +281,12 @@ namespace Part_6___Loops
 
             static void Program4()
             {
+                Random generator = new Random();
+
+                int minNum, maxNum, randNum, num;
+                bool done;
+                done = false;
+                num = 1;
 
                 Console.WriteLine();
                 Console.WriteLine("Running Random Numbers... ");
@@ -241,7 +294,54 @@ namespace Part_6___Loops
                 Console.WriteLine();
                 Console.WriteLine();
 
+                do
+                {
+                    Console.Write("Enter a MINIMUM value:  ");
+                    if (!Int32.TryParse(Console.ReadLine(), out minNum))
+                    {
+                        Console.WriteLine("Invalid Input. ");
+                    }
+                    else
+                    {
+                        done = true;
+                    }
 
+                } while (!done);
+
+                Console.WriteLine();
+                Thread.Sleep(500);
+
+                do
+                {
+                    Console.Write("Enter a MAXIMUM value:  ");
+                    if (!Int32.TryParse(Console.ReadLine(), out maxNum))
+                    {
+                        Console.WriteLine("Invalid Input. ");
+                    }
+                    else
+                    {
+                        done = true;
+                    }
+
+                } while (!done);
+
+                Console.WriteLine();
+                Thread.Sleep(500);
+
+                maxNum += 1;
+
+                for (int i = 1; i < 26; i++)
+                {
+                    Console.Write($"{num}. ");
+                    num += 1;
+                    randNum = generator.Next(minNum, maxNum);
+                    Console.WriteLine(randNum);
+                    Thread.Sleep(101);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Program finished, press ENTER to continue to menu");
+                Console.ReadLine();
             }
 
 // PROGRAM FIVE -------------------------------------------------------------------------------------------------------------------------------------
@@ -255,10 +355,226 @@ namespace Part_6___Loops
                 Console.WriteLine();
                 Console.WriteLine();
 
+                int bet, roll1, roll2, sum;
+                double betAmount, accountBalance;
+                bool done, done1;
+                string answer;
+                done = false;
+                done1 = false;
+                accountBalance = 100.00;
 
+                Die die1 = new Die();
+
+                Console.WriteLine("Dice Game --");
+                Console.WriteLine();
+                Thread.Sleep(500);
+                while (done != true)
+                {
+                    Console.WriteLine("In this game, you will bet on the outcome of 2 dice being rolled.");
+                    Console.WriteLine();
+                    Thread.Sleep(2000);
+                    Console.WriteLine($"Your account balance is: ${accountBalance}");
+                    Console.WriteLine();
+                    Thread.Sleep(1000);
+
+                    // BET ----------------------------------------------------------------------------------------------------------------------------------
+                    do
+                    {
+                        Console.WriteLine("Your bet options are: ");
+                        Console.WriteLine("1 - Doubles, 2 - not Doubles, 3 - Even Sum, 4 - Odd Sum");
+                        Console.WriteLine();
+                        Thread.Sleep(500);
+                        Console.Write("YOUR BET:  ");
+                        if (Int32.TryParse(Console.ReadLine(), out bet))
+                        {
+                            if ((bet != 1 && bet != 2 && bet != 3 && bet != 4))
+                            {
+                                Console.WriteLine();
+                                Thread.Sleep(500);
+                                Console.WriteLine("Invalid Input");
+                                Console.WriteLine();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            Thread.Sleep(500);
+                            Console.WriteLine("Invalid Input");
+                            Console.WriteLine();
+                        }
+                    } while (bet != 1 && bet != 2 && bet != 3 && bet != 4);
+
+                    // BET AMOUNT ---------------------------------------------------------------------------------------------------------------------------
+                    do
+                    {
+                        Console.WriteLine();
+                        Console.Write("YOUR BET AMOUNT:  $");
+                        if (Double.TryParse(Console.ReadLine(), out betAmount))
+                        {
+                            done1 = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            Thread.Sleep(500);
+                            Console.WriteLine("Invalid Input");
+                        }
+                    } while (done1 != true);
+
+                    betAmount = Math.Round(betAmount, 2);
+                    if (betAmount < 0)
+                    {
+                        betAmount = 0.00;
+                    }
+                    else if (betAmount > accountBalance)
+                    {
+                        betAmount = accountBalance;
+                    }
+
+                    // ROLLING ------------------------------------------------------------------------------------------------------------------------------
+                    Console.WriteLine();
+                    Console.Write("Press ENTER to roll ");
+                    Console.ReadLine();
+                    Console.WriteLine();
+                    Console.WriteLine("Rolling...");
+                    Thread.Sleep(1500);
+                    Console.WriteLine();
+
+                    Console.WriteLine("Die 1: ");
+                    die1.RollDie();
+                    die1.DrawRoll();
+                    roll1 = die1.Roll;
+                    Thread.Sleep(1000);
+
+                    Console.WriteLine("Die 2: ");
+                    die1.RollDie();
+                    die1.DrawRoll();
+                    roll2 = die1.Roll;
+                    Console.WriteLine();
+
+                    sum = roll1 + roll2;
+
+                    // DOUBLES ------------------------------------------------------------------------------------------------------------------------------
+                    if (bet == 1)
+                    {
+                        Console.WriteLine("Your bet was Doubles: ");
+
+                        if (roll1 == roll2)
+                        {
+                            Console.WriteLine("YOU WIN!");
+                            accountBalance = accountBalance + betAmount * 2;
+                            Console.WriteLine($"You bet ${betAmount} on Doubles, and won ${betAmount * 2}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("YOU LOSE!");
+                            accountBalance = accountBalance - betAmount;
+                            Console.WriteLine($"You bet ${betAmount} on Doubles, and lost ${betAmount}.");
+                        }
+                        Math.Round(accountBalance, 2);
+                        Console.WriteLine($"Your account now stands at ${accountBalance}.");
+                    }
+
+                    // NOT DOUBLES --------------------------------------------------------------------------------------------------------------------------
+                    else if (bet == 2)
+                    {
+                        Console.WriteLine("Your bet was Not Doubles: ");
+
+                        if (roll1 != roll2)
+                        {
+                            Console.WriteLine("YOU WIN!");
+                            accountBalance = accountBalance + betAmount * 0.5;
+                            Console.WriteLine($"You bet ${betAmount} on Not Doubles, and won ${betAmount / 2}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("YOU LOSE!");
+                            accountBalance = accountBalance - betAmount;
+                            Console.WriteLine($"You bet ${betAmount} on Not Doubles, and lost ${betAmount}.");
+                        }
+                        Math.Round(accountBalance, 2);
+                        Console.WriteLine($"Your account now stands at ${accountBalance}.");
+                    }
+
+                    // EVEN SUM -----------------------------------------------------------------------------------------------------------------------------
+                    else if (bet == 3)
+                    {
+                        Console.WriteLine("Your bet was Even Sum: ");
+
+                        if (sum % 2 == 0)
+                        {
+                            Console.WriteLine("YOU WIN!");
+                            accountBalance = accountBalance + betAmount;
+                            Console.WriteLine($"You bet ${betAmount} on Even Sum, and won ${betAmount}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("YOU LOSE!");
+                            accountBalance = accountBalance - betAmount;
+                            Console.WriteLine($"You bet ${betAmount} on Even Sum, and lost ${betAmount}.");
+                        }
+                        Math.Round(accountBalance, 2);
+                        Console.WriteLine($"Your account now stands at ${accountBalance}.");
+                    }
+
+                    // ODD SUM ------------------------------------------------------------------------------------------------------------------------------
+                    else if (bet == 4)
+                    {
+                        Console.WriteLine("Your bet was Odd Sum: ");
+
+                        if (sum % 2 == 0)
+                        {
+                            Console.WriteLine("YOU LOSE!");
+                            accountBalance = accountBalance - betAmount;
+                            Console.WriteLine($"You bet ${betAmount} on Odd Sum, and lost ${betAmount}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("YOU WIN!");
+                            accountBalance = accountBalance + betAmount;
+                            Console.WriteLine($"You bet ${betAmount} on Odd Sum, and won ${betAmount}.");
+                        }
+                        Math.Round(accountBalance, 2);
+                        Console.WriteLine($"Your account now stands at ${accountBalance}.");
+                    }
+
+
+                    // QUIT -------------------------------------------------------------------------------------------------------------------------------------
+                    Console.WriteLine();
+                    Console.Write("Would you like to quit?(Y/N) ");
+                    answer = Console.ReadLine().ToUpper();
+                    if (answer == "Y" || answer == "YES")
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Game Over.");
+                        Thread.Sleep(1000);
+                        done = true;
+                    }
+                    else if (answer == "N" && accountBalance < 0 || answer == "NO" && accountBalance < 0)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Insufficient funds");
+                        Thread.Sleep(1000);
+                        Console.WriteLine("Game Over.");
+                        Thread.Sleep(1000);
+                        done = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Okay, lets continue...");
+                        Thread.Sleep(1000);
+                        Console.WriteLine();
+                        Console.WriteLine("----------------------------------------------------------------------------------------------------");
+                        Console.WriteLine();
+                        Thread.Sleep(1000);
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Program finished, press ENTER to continue to menu");
+                Console.ReadLine();
             }
-
-
         }   
     }
 }
